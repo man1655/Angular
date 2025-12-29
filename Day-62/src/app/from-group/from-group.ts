6d5d62368466a84923e5cd09ad143250b3aabb8f
man1655 <manpatel1655@gmail.com>
2025-12-29 16:31:13 +0530

    ReactiveForm Validation

--- Day-62/src/app/from-group/from-group.ts
+++ Day-62/src/app/from-group/from-group.ts
@@ -1,18 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { form } from '@angular/forms/signals';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { form, validate } from '@angular/forms/signals';
 import { first } from 'rxjs';
 
 @Component({
   selector: 'app-from-group',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule,NgIf],
   templateUrl: './from-group.html',
   styleUrl: './from-group.css',
 })
 export class FromGroup {
   LoginForm: FormGroup = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    firstName: new FormControl('',[Validators.required,Validators.minLength(3)]),
+    lastName: new FormControl('',[Validators.required,Validators.maxLength(20)]),
   });
   submitForm(){
     console.log(this.LoginForm.value)
@@ -23,5 +24,10 @@ export class FromGroup {
       lastName:'bye '
     })
   }
-
+  get  firstName(){
+    return this.LoginForm.get('firstName')
+  }
+  get lastName(){
+    return this.LoginForm.get('lastName')
+  }
 }
