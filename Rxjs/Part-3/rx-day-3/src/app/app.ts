7e26485f99d1e24805b11bcde13660fa24dcf513
man1655 <manpatel1655@gmail.com>
2026-01-19 11:34:16 +0530

    changes

--- Rxjs/Part-3/rx-day-3/src/app/app.ts
+++ Rxjs/Part-3/rx-day-3/src/app/app.ts
@@ -6,20 +6,20 @@ import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,AsyncPipe],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.html',
-  standalone:true,
+  standalone: true,
   styleUrl: './app.css',
 })
+
 export class App {
   studentname$ = new Subject<string>();
   number$ = new BehaviorSubject<number>(0);
 
   private subscription!: Subscription;
-  private userServices=inject(Userservices)
+  private userServices = inject(Userservices);
+
   user$ = this.userServices.getUser();
-
-
   constructor() {
     this.number$.subscribe((val) => {
       console.log('subscriber 1', val);
@@ -27,6 +27,8 @@ export class App {
     this.number$.next(1);
     this.number$.next(2);
     this.number$.next(3);
+
+
     setTimeout(() => {
       this.studentname$.next('Angular 20');
     }, 3000);
@@ -34,16 +36,16 @@ export class App {
       console.log('sub2', val);
     });
   }
-  refresh(){
-    this.user$=this.userServices.getUser();
+
+  refresh() {
+    this.user$ = this.userServices.getUser();
   }
+
   ngOnInit() {
     this.subscription = this.studentname$.subscribe((res) => {
       console.log(res);
     });
   }
-
-
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
